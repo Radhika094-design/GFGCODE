dc705f07b1fafffe32ffde95c9b95fd623dff9a1
Radhika <radheya981@gmail.com>
2025-07-18 11:55:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+  void DFS(vector<vector<char>> &mat,int i ,int  j  , int r ,int c){
+    if(i==r || j == c || i<0 || j<0 || mat[i][j]!='L'){
+            return ; 
+        }
+        mat[i][j] = 'R';
+        DFS(mat,i- 1,j-1,r,c);
+        DFS(mat,i-1,j+1,r,c);
+        DFS(mat,i+1,j+1,r,c);
+        DFS(mat,i+1,j-1,r,c);
+        DFS(mat,i,j+1,r,c);
+        DFS(mat,i-1,j,r,c);
+        DFS(mat,i,j-1,r,c);
+        DFS(mat,i+1,j,r,c);  
+      
+  }
+    int countIslands(vector<vector<char>>& grid) {
+        // Code here
+         int r = grid.size();
+        int  c = grid[0].size(); // take size of matrix 
+        int ans = 0; // for count no of islands 
+        for(int i = 0 ;i<r;i++){
+            for(int j = 0 ; j < c ; j++){
+                if(grid[i][j] == 'L'){     //transeverse when it is only 1 
+                    DFS(grid , i , j , r, c);
+                    ans++;    // and make interally 0 so that not visited again the nodes 
+                }
+            }
+        }
+        return ans;
+    
+        
+    }
+};
