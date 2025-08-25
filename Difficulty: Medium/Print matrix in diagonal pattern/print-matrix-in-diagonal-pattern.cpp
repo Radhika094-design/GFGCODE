745ab767678dde70439e366d59417976ec0332d7
Radhika <radheya981@gmail.com>
2025-08-25 22:01:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print matrix in diagonal pattern/print-matrix-in-diagonal-pattern.cpp
@@ -0,0 +1,29 @@
+/*You are required to complete this method */
+
+class Solution {
+  public:
+    vector<int> matrixDiagonally(vector<vector<int>>& matrix) {
+        // Your code here
+         if (matrix.empty() || matrix[0].empty()) return {};
+
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> result(m * n);
+        int row = 0, col = 0;
+
+        for (int i = 0; i < m * n; i++) {    // m*n size of array to store travesral
+            result[i] = matrix[row][col];
+        // if the row + col is even  move up right 
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) row++;   // if last column
+                else if (row == 0) col++; //if at frst row
+                else { row--; col++; }
+            } else {   //if sum is odd move down left
+                if (row == m - 1) col++;   // if at last row 
+                else if (col == 0) row++;  // if we at frst column.
+                else { row++; col--; }
+            }
+        }
+        // return until m*n elements filled in result
+        return result;
+    }
+};
